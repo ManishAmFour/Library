1c5a998d9602c073f72734374d68850ff6046e08
Manish Tewatia <ManishTewatia3020@gmail.com>
2024-11-24 14:41:28 +0530

    Made changes regarding the functionality of books

--- functions.js
+++ functions.js
@@ -1,35 +1,36 @@
 let Book = [{BookName: `The Hobbit`,
              Author: `J.R.R Tolkien`,   
              Pages: `295 pages`,       
-             ReadingStatus : `You've read it!`       
+             ReadingStatus : `You've read it!`,
 }]
 
 
-GenerateThePage();
 
 function GenerateThePage(){
 
-    console.log(`runned`)
-
     let HTML = ``;
 
-Book.forEach((IndBook)=>{
+    let indexNo;
 
-    HTML += `<div>
+Book.forEach((IndBook,index)=>{
+
+    indexNo = index
+
+    HTML = `<div class="HTML-${index}"  data-index-name = "${indexNo}">
     ${IndBook.BookName}
     ${IndBook.Author}
     ${IndBook.ReadingStatus}
-    </div>`
+    ${indexNo}
 
-})
+    </div>`}
+
+)
+
 document.querySelectorAll(`.add-button-books`).forEach((fillIn)=>{
 
-    fillIn.innerHTML = `<div class="books">${HTML}</div>`
-
-
+    fillIn.innerHTML = `<div class="books">${HTML}</div>`    
 
 })
-
 
 document.querySelector(`.book-section`).innerHTML += `<button class="books add-button-books">Add New Book</button>`;
 
@@ -37,11 +38,9 @@ document.querySelectorAll(`.add-button-books`).forEach((ButtonInput)=>{
 
 ButtonInput.addEventListener(`click`,()=>{
 
-    console.log(`bhgbi`)
 
     if(ButtonInput.innerText === `Add New Book`){
 
-        console.log(`andar`)
     
         ButtonInput.innerHTML = `BookName:- <input class="input-bar input-BookName">
         Author:- <input class="input-bar input-Author">
@@ -53,18 +52,20 @@ ButtonInput.addEventListener(`click`,()=>{
             input.focus();
 
 
+
             input.addEventListener(`keydown`,(event)=>{
     
                 if(event.key === `Enter`){
-        
+
+
+
+                
                     Book.push({
                         BookName: document.querySelector(`.input-BookName`).value,
                         Author:  document.querySelector(`.input-Author`).value,
-                        Pages: document.querySelector(`.input-Pages`).value
-        
+                        Pages: document.querySelector(`.input-Pages`).value, 
                     })
         
-                    console.log(Book)
                     GenerateThePage();
                     ButtonInput.innerText = `Add New Book`;
                     //document.querySelector(``)
@@ -101,6 +102,7 @@ ButtonInput.addEventListener(`click`,()=>{
 
 
 
+GenerateThePage();
 
 
 
